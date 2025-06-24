62cda0771cb9dbfa42e9457b9cfbb72060e96943
Josue Bladimir Morales Llanganate <76928507+MrT-coder@users.noreply.github.com>
2025-06-23 23:58:50 -0500

    feat: add project base

---
+++ servicio-ordenes/src/main/java/com/distribuidora/servicio_ordenes/ServicioOrdenesApplication.java
@@ -0,0 +1,13 @@
+package com.distribuidora.servicio_ordenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioOrdenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioOrdenesApplication.class, args);
+	}
+
+}
